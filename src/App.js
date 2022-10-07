e98d17f2b15016d6146ebf5f47470ae3d28003cc
Luis Rosales <luismrosales2@gmail.com>
2022-10-07 01:33:27 -0400

    finished project

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { useState, useRef } from "react";
+// Components
+import Song from "./components/Song";
+import Player from "./components/Player";
+import Library from "./components/Library";
+import Nav from "./components/Nav";
+
+// Styles
+import "./styles/app.scss";
+import data from "./data";
+
+function App() {
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+    });
+  };
+  // STATE
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+    animationPercentage: 0,
+  });
+  // REF
+  const audioRef = useRef(null);
+  // state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [libraryStatus, setLibraryStatus] = useState(false);
+
+  const songEndHandler = async () => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    if (isPlaying) {
+      setTimeout(() => {
+        audioRef.current.play();
+      }, 100);
+    }
+  };
+  return (
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Song currentSong={currentSong} />
+      <Player
+        setCurrentSong={setCurrentSong}
+        songs={songs}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
+        audioRef={audioRef}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+        setSongs={setSongs}
+      />
+      <Library
+        audioRef={audioRef}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        isPlaying={isPlaying}
+        setSongs={setSongs}
+        libraryStatus={libraryStatus}
+      />
+      <audio
+        onLoadedMetadata={timeUpdateHandler}
+        onTimeUpdate={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+        onEnded={songEndHandler}
+      ></audio>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LibrarySong.js
@@ -0,0 +1,46 @@
+import React from "react";
+const LibrarySong = ({
+  song,
+  songs,
+  setCurrentSong,
+  id,
+  audioRef,
+  isPlaying,
+  setSongs,
+}) => {
+  const songSelectHandler = async () => {
+    const selectedSong = songs.filter((state) => state.id === id);
+    await setCurrentSong(selectedSong[0]);
+    // ADD ACTIVE STATE
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+    // CHECK IF THE SONG IS PLAYING
+    if (isPlaying) audioRef.current.play();
+  };
+  return (
+    <div
+      onClick={songSelectHandler}
+      className={`library-song ${song.active ? "selected" : ""}`}
+    >
+      <img src={song.cover} alt="Album cover"></img>
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default LibrarySong;
